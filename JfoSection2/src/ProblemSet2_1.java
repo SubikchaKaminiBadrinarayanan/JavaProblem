beaf7aad74d72b84ac5c109a019ee1c5d51fd63f
SubikchaKaminiBadrinarayanan <146056026+SubikchaKaminiBadrinarayanan@users.noreply.github.com>
2024-06-26 20:24:05 +0530

    Add files via upload

---
+++ JfoSection2/src/ProblemSet2_1.java
@@ -0,0 +1,16 @@
+/*Task
+Use 8 print statements to recreate the smiley face above. Your art will rely on only a single character, besides space, such as X or #. 
+The ProblemSet2_1 project is available to help you get st*/
+public class ProblemSet2_1 {
+
+	public static void main(String[] args) {
+		System.out.println("  XXXXX  ");
+        System.out.println(" X     X ");
+        System.out.println("X  O O  X");
+        System.out.println("X       X");
+        System.out.println("X   ^   X");
+        System.out.println(" X     X ");
+        System.out.println("  XXXXX  ");
+	}
+
+}
---
+++ JfoSection2/src/ProblemSet2_2.java
@@ -0,0 +1,20 @@
+/*Use print statements to create your own beautiful original ASCII art. Use comments to describe what your image is depicting.
+It’s ok for your art to rely on only a single character, besides space, such as X or #. But you’re encouraged to use a few different 
+characters in your design, like in the cat example from class.
+Your art must also:
+• Use at least 8 print statements
+• Be at least 8 characters wide
+• Use at least 20 characters that aren’t space
+You’re welcome to create another cat. However, this image must look significantly different from the example used in class. Similarly, 
+you’re welcome to create another face, but it must look significantly different from the face in the previous practice (it’s way too easy to 
+turn the smiley face into a frowny face).*/
+public class ProblemSet2_2 {
+    public static void main(String[] args) {
+        System.out.println("    |\\_/|   ");
+        System.out.println("  \\| o o |/ ");
+        System.out.println("   |  ~  |  ");
+        System.out.println("  __\\ - /__ ");
+        System.out.println(" /  `---'  \\");
+        System.out.println("/___________\\");
+    }
+}
